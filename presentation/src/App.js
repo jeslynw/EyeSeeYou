389bdab475c76b9e5bf59cc648b55db22db642c6
jeslynw <wangsajeslyn@gmail.com>
2024-08-11 17:51:26 +0800

    added tailwind.config.js

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='text-5xl'>
-      <p>Hello</p>
+    <div className='text-5xl text-red-600'>
+      <p>BYE</p>
     </div>
   );
 }
