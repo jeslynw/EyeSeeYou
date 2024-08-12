3b74f625b93b3b864d2864ac512b7cce8d15b3bb
Roy <roydennchai@gmail.com>
2024-08-12 13:49:20 +0800

    First commit to my branch

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,10 +1,24 @@
-// import logo from './logo.svg';
+-- Active: 1712643940420@@127.0.0.1@3306@eyeseeyou
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App text-7xl text-yellow-600">
-      Helloss
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React, fuck this shit
+        </a>
+      </header>
     </div>
   );
 }
