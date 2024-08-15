45f1aeaa1ef3f100daa8c085b8921bd5243ceef4
Ervina Wijaya <ervinawijaya04@gmail.com>
2024-08-15 15:27:30 +0800

    -

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,14 +1,15 @@
 // import logo from './logo.svg';
 import "./App.css";
+import LoginUI from "./pages/LoginUI";
 import UpdateAccountDetailsUI from "./pages/UpdateAccountDetailsUI";
 import ViewAccountDetailsUI from "./pages/ViewAccountDetailsUI";
 
 export default function App() {
   return (
     <div>
-      <LoginUI />
-      {/* <UpdateAccountDetailsUI /> */}
+      {/* <LoginUI /> */}
       <ViewAccountDetailsUI />
+      {/* <UpdateAccountDetailsUI /> */}
     </div>
   );
 }
