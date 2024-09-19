807e7740803ff5181c6c6a5ee73c58bf3fa000e8
jeslynw <wangsajeslyn@gmail.com>
2024-09-19 21:59:25 +0800

    trending attacks

--- presentation/src/pages/ViewTrendingAttacksUI.js
+++ presentation/src/pages/ViewTrendingAttacksUI.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from "../components/Header";
 import { useTheme } from "../components/ThemeProvider";
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import { Link } from "react-router-dom";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import axios from "axios";
-// import Typography from '@mui/material/Typography';
+import TrendingAttacks from '../components/TrendingAttacks';
+
 
 function TrendingAttacksUI() {
-
-    //debugging for user
     const access_token = sessionStorage.getItem('accesstoken');
     const refresh_token = sessionStorage.getItem('refreshtoken');
     if (access_token) {
@@ -39,8 +38,54 @@ function TrendingAttacksUI() {
     const showDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
     const showTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
     const currentDate = showDate + ' , ' + showTime
-    // event.preventDefault();
-    // console.info('You clicked a breadcrumb.');
+
+    const [error, setError] = useState(null);
+    const [trendAttackCategory, setTrendAttackCategory] = useState([]);
+    const [trendAttackData, setTrendAttackData] = useState([]);
+    console.log("trendAttackCategory:", trendAttackCategory);
+    console.log("trendAttackData:", trendAttackData);
+
+    useEffect(() => {
+        const access_token = sessionStorage.getItem('accesstoken');
+
+        const fetchData = () => {
+            axios.get('http://127.0.0.1:5000/nadashboard', {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    //  trending attacks data
+                    const alertClasses = response.data.trending_attacks.map(alert => alert.class);
+                    const classCounts = response.data.trending_attacks.map(alert => alert.count);
+                    const sortedData = alertClasses.map((c, i) => ({ class: c, count: classCounts[i] })).sort((a, b) => b.count - a.count);
+                    const sortedCategories = sortedData.map(d => d.class);
+                    const sortedSeriesData = sortedData.map(d => d.count);
+                    setTrendAttackCategory(sortedCategories);
+                    setTrendAttackData([{ data: sortedSeriesData }]);
+
+
+                    setTrendAttackCategory(sortedCategories);
+                    setTrendAttackData([{ data: sortedSeriesData }]);
+
+                } else {
+                    setError('No data available');
+                }
+                const temp = response.data.alert_classes;
+                console.log("Alert classes:", temp);
+            })
+            .catch(error => {
+                console.error('Error fetching threat info:', error);
+                setError('Error fetching data');
+            });
+        } 
+        fetchData();
+        const interval = setInterval(fetchData, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
 
     const breadcrumbItems = [
         {path: '/nadashboard', name:'Dashboard'},
@@ -61,7 +106,7 @@ function TrendingAttacksUI() {
                     <Breadcrumbs separator={<NavigateNextIcon fontSize="small" color="primary" />} aria-label="breadcrumb">
                     {breadcrumbItems.map((item) => (
                         <Link
-                        className='text-[#000] dark:text-[#ffffff]'
+                        className='text-[#413c3c9d] dark:text-[#ffffff79] text-sm font-light'
                         to = {item.path}
                         underline="hover"
                         onClick = {item.onClick}
@@ -80,8 +125,8 @@ function TrendingAttacksUI() {
                     {/* Bar Chart */}
                     <div className="border border-[#e7e7e7] dark:border-[#353535] shadow-md rounded-xl px-4 py-4 bg-white dark:bg-transparent">
                         <p className="pb-3 text-sm md:text-base">Trending Attacks Bar Chart</p>
-                        <div className="h-96">
-
+                        <div className="h-96 px-8">
+                            <TrendingAttacks trendAttackCategory={trendAttackCategory} trendAttackData={trendAttackData} width={1000} height={380} />
                         </div>
                     </div>
 
